b82633be35adc62edd207d4c84f87cbf9af7bc00
danielqueirozc <danielqueiroz.job@gmail.com>
2026-05-03 15:41:27 -0300

    test: create authenticate test

---
+++ api/src/http/controllers/authenticate.controller.e2e-spec.ts
@@ -0,0 +1,44 @@
+import { AppModule } from "@/app.module"
+import { PrismaService } from "@/database/prisma/prisma.service"
+import type { INestApplication } from "@nestjs/common"
+import { Test } from "@nestjs/testing"
+import { hash } from "bcryptjs"
+import request from 'supertest'
+
+describe('Authenticate (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+   beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /sessions', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'John Snow',
+        email: 'johnsnow817@gmail.com',
+        password: await hash('1234567', 8),
+      }
+    })
+
+    const response = await request(app.getHttpServer()).post('/sessions').send({
+      email: 'johnsnow817@gmail.com',
+      password: '1234567',
+    })
+
+    // expect(response.statusCode).toBe(201)
+
+     expect(response.body).toEqual({
+      access_token: expect.any(String),
+    })
+  })
+})
