c3a49530b8200922b2c8cf4668f236937a406402
danielqueirozc <danielqueiroz.job@gmail.com>
2026-05-03 15:38:32 -0300

    test: create Create Scheduling

---
+++ api/src/http/controllers/create-scheduling.controller.e2e-spec.ts
@@ -0,0 +1,53 @@
+import { AppModule } from "@/app.module"
+import { PrismaService } from "@/database/prisma/prisma.service"
+import type { INestApplication } from "@nestjs/common"
+import { Test } from "@nestjs/testing"
+import request from 'supertest'
+
+describe("Create Scheduling", () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+
+    // Cria o serviço e o funcionário necessários para o teste
+    const service = await prisma.service.create({
+      data: { name: 'Pintura', description: 'Pintura interna e externa' },
+    })
+
+    const employee = await prisma.employee.create({
+      data: {
+        name: 'Fernanda Oliveira',
+        function: 'Pintora',
+        status: 'ACTIVE',
+        email: 'fernanda.oliveira@avila.com',
+        phone: '(11) 91111-0005',
+      },
+    })
+
+    await prisma.employeeService.create({
+      data: { employeeId: employee.id, serviceId: service.id },
+    })
+  })
+
+  test('[POST] /scheduling', async () => {
+    const response = await request(app.getHttpServer()).post('/scheduling').send({
+      serviceName: 'Pintura',
+      serviceDate: new Date(),
+      clientName: 'Thiago',
+      clientEmail: 'thiago@test.com',
+      clientPhone: '40028922',
+      clientAddress: 'Rua 1',
+    })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
