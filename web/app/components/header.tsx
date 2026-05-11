e70afffd3e3792bb6e1654947f73c6746eeba1a6
danielqueirozc <danielqueiroz.job@gmail.com>
2026-05-11 17:55:02 -0300

    feat: create header component

--- web/app/components/header.tsx
+++ web/app/components/header.tsx
@@ -1,7 +1,23 @@
+import { House, LogIn } from "lucide-react"
+import Link from "next/link"
+
 export function Header() {
   return (
-    <header>
-      
+    <header className="text-white bg-blue pt-6 pb-4 px-4 flex flex-col gap-2">
+      <div className="flex gap-2">
+        <House size={28} />
+        <h1 className="text-xl font-medium">Ávila Manutenção e Reparos</h1>
+      </div>
+
+     <span className="text-gray-200">Agende seu serviço de forma rápida e fácil</span>
+
+    <Link 
+      className="fixed right-4 flex items-center gap-2 px-4 py-2 bg-white text-black text-xs font-medium rounded-lg"
+      href='/login'
+    >
+      <LogIn size={14} />
+        Acesso Funcionários
+      </Link>
     </header>
   )
 }
