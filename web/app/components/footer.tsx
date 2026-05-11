32702a815f574b5a99cd5f4df594c1675f5ec56b
danielqueirozc <danielqueiroz.job@gmail.com>
2026-05-11 17:54:33 -0300

    feat: create footer component

---
+++ web/app/components/footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+  return (
+    <footer className="flex flex-col p-8 gap-4 bg-blue text-gray-50 text-center mt-25">
+      <p>© 2025 Ávila Manutenção e Reparos - Todos os direitos reservados</p>
+      <p>Dúvidas? Entre em contato: (00) 0000-0000 | contato@avila.com.br</p>
+    </footer>
+  )
+}
