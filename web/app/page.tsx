55cfcbd9109101b8b48280c18703e165154e1865
danielqueirozc <danielqueiroz.job@gmail.com>
2026-05-11 17:53:38 -0300

    feat: create const steps

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,6 +1,13 @@
 import { Header } from "./components/header";
 
 export default function Home() {
+  const steps = [
+  { label: "Serviço" },
+  { label: "Data/Hora" },
+  { label: "Dados" },
+  { label: "Confirmação" },
+]
+
   return (
    <div className="">
     <Header />
