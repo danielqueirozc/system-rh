06e92e1777d0ea2135bf4a414f79f86ca3c67147
danielqueirozc <danielqueiroz.job.com>
2026-05-16 13:40:42 -0300

    feat: create Date page

---
+++ web/app/components/date/page.tsx
@@ -0,0 +1,10 @@
+export default function Date() {
+  return (
+    <div className="flex flex-col px-4 mt-5">
+      <div className="flex flex-col gap-2 text-center">
+        <h1 className="text-black text-lg font-semibold">Escolha Data e Horário</h1>
+        <p className="text-gray-500 text-sm font-medium">Selecione o melhor dia e horário para o serviço de encanamento</p>
+      </div>
+    </div>
+  )
+}
