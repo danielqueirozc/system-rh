e0d94883079b3eabea18a7d07bfb9919a01b1cd4
danielqueirozc <danielqueiroz.job@gmail.com>
2026-05-26 17:40:31 -0300

    feat: create SchedulingService

---
+++ web/lib/axios.ts
@@ -0,0 +1,36 @@
+import type { SchedulingProps } from "@/context/scheduling-store"
+import axios from "axios"
+
+export const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL
+})
+
+// api.interceptors.request.use((config) => {
+//   const token = useAuthStore.getState().token
+
+//    if (token) {
+//     config.headers.Authorization = `Bearer ${token}`
+//   }
+
+//   return config
+// })
+
+// api.interceptors.response.use(
+//  (response) => response,
+//   (error) => {
+//     if (error.response?.status === 401) {
+//       // Usa a action logout da própria store
+//       useAuthStore.getState().logout()
+//       window.location.href = '/login'
+//     }
+//     return Promise.reject(error)
+//   }
+// )
+
+export const SchedulingService = {
+  create: async (data: SchedulingProps) => {
+    const response = await api.post('/scheduling', data)
+
+    return response
+  }
+}
