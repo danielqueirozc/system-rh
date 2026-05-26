2b1bb0400c42e32d8840eac4f7916bee4d556337
danielqueirozc <danielqueiroz.job@gmail.com>
2026-05-25 22:00:01 -0300

    style: add mt-25

--- web/app/components/footer.tsx
+++ web/app/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="flex flex-col p-8 gap-4 bg-blue text-gray-50 text-center mt-25">
+    <footer className="flex flex-col p-8 gap-4 bg-blue text-gray-50 text-center">
       <p>© 2025 Ávila Manutenção e Reparos - Todos os direitos reservados</p>
       <p>Dúvidas? Entre em contato: (00) 0000-0000 | contato@avila.com.br</p>
     </footer>
