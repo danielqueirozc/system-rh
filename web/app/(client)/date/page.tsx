9efaa4e17dc11e1b9bbd1492cae5772c19e0c492
danielqueirozc <danielqueiroz.job@gmail.com>
2026-05-25 21:52:52 -0300

    feat: create DatePage

---
+++ web/app/(client)/date/page.tsx
@@ -0,0 +1,94 @@
+'use client'
+
+import { useState } from "react"
+import Link from "next/link"
+import { useSchedulingStore } from "@/context/scheduling-store"
+import { Calendar } from "@/app/components/ui/calendar"
+import { Button } from "@/app/components/ui/button"
+import { useRouter } from "next/navigation"
+
+export default function DatePage() {
+  const [date, setDate] = useState<Date | undefined>(new Date())
+
+  const { setDateScheduling, setStep } = useSchedulingStore()
+
+  const router = useRouter()
+
+  const times = [
+   "08:00",
+   "09:00",
+   "10:00",
+   "11:00",
+   "13:00",
+   "14:00",
+   "15:00",
+   "16:00",
+   "17:00" 
+  ]
+
+  function handleConfirm(selectedTime: string) {
+    if (!date) return
+
+    const [hours, minutes] = selectedTime.split(":")
+    const conbined = new Date(date)
+    conbined.setHours(Number(hours), Number(minutes))
+
+    setDateScheduling(conbined)
+    setStep(3)
+    router.push('/data')
+  }
+
+  return (
+    <div className="flex flex-col mt-5 px-4 pb-25 text-gray-800 bg-[#F9F9F9]">
+      <div className="flex flex-col gap-2 text-center mt-5">
+        <h1 className="text-black text-lg font-semibold">Escolha Data e Horário</h1>
+        <p className="text-gray-500 text-sm font-medium">Selecione o melhor dia e horário para o serviço de encanamento</p>
+      </div>
+
+      <div className="bg-white border border-purple rounded-lg flex flex-col gap-16 p-8 mt-10">
+        <span>Selecione a data</span>
+
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-lg border border-purple w-full"
+          captionLayout="dropdown"
+          disabled={day => {
+            const today = new Date()
+            today.setHours(0,0,0,0)
+            return day <= today
+          }}
+        />
+      </div>
+
+      <div className="bg-white border border-purple rounded-lg flex flex-col gap-12 p-7 mt-10">
+        <span>Horários Disponíveis</span>
+
+        <div className="grid grid-cols-3 gap-3">
+          {times.map(time => (
+            <Button
+              key={time}
+              variant="date"
+              className="px-10 py-2"
+              onClick={() => handleConfirm(time)}
+            >
+              {time}
+            </Button>
+          ))}
+        </div>
+      </div>
+
+      <div className="w-full flex justify-center mt-7">
+        <Link href='/'>
+          <Button 
+            variant="date"
+            className="px-8.5 py-2"
+          >
+            Voltar
+          </Button>
+        </Link>
+      </div>
+    </div>
+  )
+}
