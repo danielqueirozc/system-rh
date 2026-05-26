35bdb036131f790f02ba35d4d19024682f959e8f
danielqueirozc <danielqueiroz.job@gmail.com>
2026-05-26 17:39:50 -0300

    feat: create SchedulingStore

---
+++ web/context/scheduling-store.ts
@@ -0,0 +1,77 @@
+import { SchedulingService } from '@/lib/axios'
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+interface DataProps {
+    name: string
+    email: string
+    phone: string
+    address: string
+    description?: string
+}
+
+export interface SchedulingProps {
+  serviceName: string 
+  serviceDate: Date 
+  clientName: string 
+  clientEmail: string 
+  clientAddress: string 
+  clientPhone: string 
+  clientDescription: string 
+}
+
+interface SchedulingStoreType {
+  service: string
+  date: Date | null
+  data: DataProps
+  step: number
+
+  setStep: (step: number) => void
+  setService: (service: string) => void
+  setDateScheduling: (date: Date) => void
+  setData: (data: DataProps) => void
+  createScheduling: (data :SchedulingProps) => Promise<void>
+} 
+
+export const useSchedulingStore = create<SchedulingStoreType>()(
+  persist(
+    (set) => ({
+    service: '',
+    date: null,
+    data: {
+      name: '',
+      email: '',
+      phone: '',
+      address: '',
+      description: ''
+    },
+    step: 1,
+
+    setStep: (step) => set ({ step }),
+    setService: (service) => set({ service }),
+    setDateScheduling: (date) => set({ date }),
+    setData: (data) => set({ data }),
+    createScheduling: async (data) => {
+      try {
+        const response = await SchedulingService.create(data)
+
+        // set(state => ({
+
+        // }))
+      } catch (error) {
+        console.error('Erro ao criar Scheduling', error)
+        throw error
+      }
+    },
+  }),
+  {
+      name: 'scheduling-storage',
+      partialize: (state) => ({
+        service: state.service,
+        date: state.date,
+        data: state.data,
+        step: state.step,
+      }),
+    }
+  )
+)
