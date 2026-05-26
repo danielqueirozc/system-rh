438881b5442c75702b7f52b83d7f4a0bd5aecc74
danielqueirozc <danielqueiroz.job@gmail.com>
2026-05-25 21:51:40 -0300

    feat: create layout

---
+++ web/app/(client)/layout.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import type { ReactNode } from "react";
+import { Footer } from "../components/footer";
+import { Header } from "../components/header";
+import { Stepper } from "../components/stepper";
+import { useSchedulingStore } from "@/context/scheduling-store";
+
+interface ChildrenType {
+  children: ReactNode
+}
+
+export default function ClientLayout({ children }: ChildrenType) {
+  const {step} = useSchedulingStore()
+
+  const steps = [
+    { label: "Serviço" },
+    { label: "Data/Hora" },
+    { label: "Dados" },
+    { label: "Confirmação" },
+  ]
+
+  return (
+    <div className="bg-[#F9F9F9]">
+      <Header />
+      <div className="lg:w-full lg:flex lg:justify-center">
+        <Stepper steps={steps} currentStep={step} />
+      </div>
+      {children}
+      <Footer />
+    </div>
+  )
+}
