08f7b067632c83da547697edb65b1840ce02a4ff
danielqueirozc <danielqueiroz.job@gmail.com>
2026-05-31 16:12:31 -0300

    chore: add cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,10 +4,11 @@ import { ConfigService } from '@nestjs/config';
 import type { Env } from './env';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
+  app.enableCors({ origin: 'http://localhost:3001' })
   const configService: ConfigService<Env, true> = app.get(ConfigService)
   const port = configService.get('PORT', { infer: true })
 
   await app.listen(port)
 }
-bootstrap();
+bootstrap()
