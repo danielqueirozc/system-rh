0faedaf524db5186a86538f50bc75449f56c82a4
danielqueirozc <danielqueiroz.job@gmail.com>
2026-07-19 22:18:56 -0300

    feat: add shared layout for employee routes

---
+++ web/app/(employee)/layout.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { Fragment, useEffect, useState, type ReactNode } from "react";
+import { NavbarMenuDesktop } from "../components/navbar-menu-desktop";
+import { HeaderMobile } from "../components/header-mobile";
+
+interface ChildrenType {
+  children: ReactNode
+}
+
+export default function ClientLayout({ children }: ChildrenType) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return (
+    <Fragment>
+      {windowWidth >= 1024 ? (
+        <div className="flex">
+          <NavbarMenuDesktop />
+          {children}
+        </div>
+      ) : (
+        <div className="flex-col">
+          <HeaderMobile title='Dashboard' />
+          {children}
+        </div>
+      )}
+    </Fragment>
+  )
+}
