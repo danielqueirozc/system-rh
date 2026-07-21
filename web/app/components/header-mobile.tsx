7ff9bbb31e2beb58d422817c573fd8593cd2356b
danielqueirozc <danielqueiroz.job.com>
2026-07-21 11:05:36 -0300

    refactor: derive HeaderMobile title from current route

--- web/app/components/header-mobile.tsx
+++ web/app/components/header-mobile.tsx
@@ -1,14 +1,22 @@
 import { NavbarMenuMobile } from "./navbar-menu-mobile";
+import { usePathname } from 'next/navigation'
 
-interface HeaderMobileProps {
-  title: string
-}
+export function HeaderMobile() {
+  const route = usePathname()
 
-export function HeaderMobile({ title }: HeaderMobileProps) {
+  const routes = [
+    { title: "Dashboard",    href: "/dashboard" },
+    { title: "Clientes",     href: "/clients" },
+    { title: "Agendamentos", href: "/appointments" },
+    { title: "Orçamentos",   href: "/budgets" },
+    { title: "Funcionários", href: "/employees" },
+    { title: "Relatórios",   href: "/reports" },
+  ]
+
   return (
      <header className="fixed top-0 left-0 w-full flex items-center gap-8 p-7 border-b border-gray-200 bg-white z-10">
       <NavbarMenuMobile />
-      <span className="text-black text-sm md:text-base">{title}</span>
+      <span className="text-black text-sm md:text-base">{routes.find(r => r.href === route)?.title}</span>
     </header>
   )
 }
