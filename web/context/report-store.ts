53c16b680ac940a0e42a3cdef3468563dba0e5a2
danielqueirozc <danielqueiroz.job@gmail.com>
2026-08-11 18:14:06 -0300

    feat(reports): wire new variation fields into report store

--- web/context/report-store.ts
+++ web/context/report-store.ts
@@ -3,9 +3,13 @@ import { create } from 'zustand'
 
 interface ReportStoreType {
     revenueTotal: number
+    totalRevenueVariation: number
     servicesRealized: number
+    realizedServicesVariation: number
     ticketMedio: number
+    TicketMedioVariation: number
     taxaConversao: number
+    taxaConversaoVariation: number,
     servicesQuantity: [
       { month: string, count: number }
     ],
@@ -25,9 +29,13 @@ interface ReportStoreType {
 export const useReportStore = create<ReportStoreType>()(
   (set) => ({
     revenueTotal: 0,
+    totalRevenueVariation: 0,
     servicesRealized: 0,
+    realizedServicesVariation: 0,
     ticketMedio: 0,
+    TicketMedioVariation: 0,
     taxaConversao: 0,
+    taxaConversaoVariation: 0,
     servicesQuantity: [{ month: '', count: 0 }],
     serviceDistribution: [{ service: '', count: 0, percentage: 0 }],
     monthlyRevenue: [{  month: '', total: 0}],
@@ -36,13 +44,20 @@ export const useReportStore = create<ReportStoreType>()(
     getReports: async (year?: number) => {
       try {
         const response = await reportService.get(year)
-        console.log(response)
-        
+        console.log(response.totalRevenueVariation)
+        console.log(response.realizedServicesVariation)
+        console.log(response.TicketMedioVariation)
+        console.log(response.taxaConversaoVariation)
+
           set({
             revenueTotal: response.revenueTotal,
+            totalRevenueVariation: response.totalRevenueVariation,
             servicesRealized: response.servicesRealized,
+            realizedServicesVariation: response.realizedServicesVariation,
             ticketMedio: response.ticketMedio,
+            TicketMedioVariation: response.TicketMedioVariation,
             taxaConversao: response.taxaConversao,
+            taxaConversaoVariation: response.taxaConversaoVariation,
             servicesQuantity: response.servicesQuantity,
             serviceDistribution: response.serviceDistribution,
             monthlyRevenue: response.monthlyRevenue,
